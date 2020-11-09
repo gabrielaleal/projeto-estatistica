ee4b78c3ba60430dec8a28a7d8094ee32029740c
caioianmelo <74146492+caioianmelo@users.noreply.github.com>
2020-11-09 09:05:55 -0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 import xlrd
 from analise_exploratoria import relatorio_analise_exploratoria, distribuicao_de_frequencia
-
+from mmq import mmq
 
 def pegar_valores_da_coluna(planilha, numero_coluna):
     return planilha.col_values(numero_coluna)[1:]
@@ -22,17 +22,23 @@ potencial = pegar_valores_da_coluna(planilha, 4)
 
 
 
-# relatorio_analise_exploratoria(metros_vendidos, "Metros Vendidos")
-# distribuicao_de_frequencia(metros_vendidos, "Metros Vendidos")
-# print('------------------------')
-# relatorio_analise_exploratoria(gastos_promocao, "Gastos Promoção")
-# distribuicao_de_frequencia(gastos_promocao, "Gastos Promoção")
+relatorio_analise_exploratoria(metros_vendidos, "Metros Vendidos")
+distribuicao_de_frequencia(metros_vendidos, "Metros Vendidos")
+print('------------------------')
+relatorio_analise_exploratoria(gastos_promocao, "Gastos PromoĂ§ĂŁo")
+distribuicao_de_frequencia(gastos_promocao, "Gastos PromoĂ§ĂŁo")
 print('------------------------')
 relatorio_analise_exploratoria(num_clientes, "Número de Clientes")
-distribuicao_de_frequencia(num_clientes, "Núm. Clientes")
+distribuicao_de_frequencia(num_clientes, "Número de Clientes")
 print('------------------------')
 relatorio_analise_exploratoria(num_concorrentes, "Número de Concorrentes")
-distribuicao_de_frequencia(num_concorrentes, "Núm. Concorrentes")
+distribuicao_de_frequencia(num_concorrentes, "Número de Concorrentes")
 print('------------------------')
-# relatorio_analise_exploratoria(potencial, "Potencial")
-# distribuicao_de_frequencia(potencial, "Potencial")
+relatorio_analise_exploratoria(potencial, "Potencial")
+distribuicao_de_frequencia(potencial, "Potencial")
+
+#imprimindo arquivo das retas
+mmq(gastos_promocao, metros_vendidos, "Gastos_Promocao")
+mmq(potencial, metros_vendidos, "Potencial")
+mmq(num_clientes, metros_vendidos, "N_Clientes")
+mmq(num_concorrentes, metros_vendidos, "N_Concorrentes")
